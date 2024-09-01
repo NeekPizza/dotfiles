86a2c329d8125a2766fd95a5c7373801cb89f7a6
Nick <neekpizza@gmail.com>
2024-09-01 17:40:48 -0500

    rename nvim folder for git issue

---
+++ config/nvim_backup/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "typescript",
+      "javascript",
+      "html",
+      "css",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
